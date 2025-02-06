df84a3fbde4db968637fae6f62e2bd2cf046af02
desarrolladorcsjt <desarrollador.csjt@gmail.com>
2025-02-06 18:53:36 -0500

    cambios

--- content/especialidades/cardiologia.php
+++ content/especialidades/cardiologia.php
@@ -20,7 +20,8 @@ background: linear-gradient(90deg, rgba(4,147,167,1) 29%, rgba(41,83,160,1) 68%)
 		<div class="row position-relative">
 			<div class="position-md-absolute top-0 col-md-7 offset-md-5 h-100">
 				<div class="bg-img-center card-img">
-				<img class="img-fluid rounded-3" style="height:400px !important" src="/images/Especialidades/Cardiología.jpg">
+					<img class="img-fluid rounded-3" style="height:400px !important"
+						src="/images/Especialidades/Cardiología.jpg">
 
 				</div>
 			</div>
@@ -52,9 +53,42 @@ background: linear-gradient(90deg, rgba(4,147,167,1) 29%, rgba(41,83,160,1) 68%)
 					contamos con la mejor tecnología de diagnóstico no invasivo: Electrocardiograma de reposo y de
 					ritmo, Electrocardiograma Holter de 24 Horas, Monitoreo Ambulatorio de Presión Arterial de 24 horas,
 					Ecocardiografía Doppler Color y Ecografía Doppler vascular.</p>
-					<?php require_once('links-cardiologia.php'); ?>
+				<?php require_once('links-cardiologia.php'); ?>
 			</div>
 		</div>
 	</div>
 </section>
+
+<div class="bard_container">
+	<div class="bard_bottom_bar" style="margin:00px 0px !important">
+		<p style="margin: 20px 480px !important">Le brindamos los siguientes <b>procedimientos de Cardiología</b></p>
+		<div class="bard_bottom_bar_content" style="padding:10px">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="bard_item">Procedimientos</div>
+						<div class="bard_item">Riesgo Quirúrgico</div>
+						<div class="bard_item">Prueba de Esfuerzo</div>
+						<div class="bard_item">Estudio de Mapa (24 Horas)</div>
+					</div>
+					<div class="col-md-4">
+						<div class="bard_item">Estudio de Holter (24 Horas)</div>
+						<div class="bard_item">Electrocardiograma</div>
+						<div class="bard_item">Ecocardiograma Doppler</div>
+					</div>
+					<div class="col-md-4">
+						<div class="bard_item">Eco Doppler Venoso MMII</div>
+						<div class="bard_item">Eco Doppler Arterial MMII</div>
+						<div class="bard_item">Eco Doppler Carotideo y Vertebral</div>
+						<div class="bard_item">Levantamiento de Observaciones</div>
+						<div class="bard_item">Informe Médico</div>
+						<div class="bard_item">Descanso Médico</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 <?php require_once('formulario.php'); ?>
--- estilos.php
+++ estilos.php
@@ -13,7 +13,7 @@
 <!-- CSS Implementing Plugins -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
- <link
+<link
 	href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
 	rel="stylesheet">
 
@@ -22,11 +22,54 @@
 <link rel="stylesheet" href="/assets/css/custom.css">
 <link rel="stylesheet" href="/assets/css/snippets.css">
 
-<link rel="stylesheet" href="/assets/vendor/dzsparallaxer/dzsparallaxer.css"> 
+<link rel="stylesheet" href="/assets/vendor/dzsparallaxer/dzsparallaxer.css">
 <link rel="stylesheet" href="/assets/vendor/dzsparallaxer/dzsscroller/scroller.css">
 <link rel="stylesheet" href="/assets/vendor/dzsparallaxer/advancedscroller/plugin.css">
- 
 
+<style>
+  .bard_container {
+    position: relative;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .bard_content {
+    flex: 1;
+    padding: 20px;
+  }
+
+  .bard_bottom_bar {
+    background-color: white;
+    padding: 10px;
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1);
+  }
+
+  .bard_bottom_bar_content {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
+  .bard_item {
+    padding: 10px;
+  }
+  .bard_bottom_bar {
+    background-color: white;
+    padding: 10px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    box-shadow: 0px -2px 5px rgba(0, 0, 0, 0.1);
+    z-index: 1000; /* Para asegurarse de que esté sobre otros elementos */
+}
+
+</style>
 <!-- #region -->
 </head>
 <!--<?php require_once('cookies.php'); ?>-->

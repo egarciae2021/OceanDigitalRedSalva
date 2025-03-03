9c0f815dc91c13b695164d2dcd9ca0a99a6afa69
desarrolladorcsjt <desarrollador.csjt@gmail.com>
2025-03-03 18:21:36 -0500

    carga 181803032025

--- content/blogs.php
+++ content/blogs.php
@@ -2,7 +2,7 @@
     <div class="container py-4 h-100">
         <div class="row h-100">
             <div class="col-md-12 h-100">
-                <iframe id="blog-wp" src="http://localhost/blog/?page_id=55" width="100%" height="100%"
+                <iframe id="blog-wp" src="https://www.redsalva.com/blog/?page_id=55" width="100%" height="100%"
                     style="border:none; display:block;margin-top:-120px !important" scrolling="no"></iframe>
             </div>
         </div>
